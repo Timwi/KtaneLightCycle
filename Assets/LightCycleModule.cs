c9334a1220ec1f2b64eb6b8cb36f8be057e9a4e6
Timwi <github@timwi.de>
2017-01-23 23:35:31 +0100

    Modeling and coroutine

--- Assets/LightCycleModule.cs
+++ Assets/LightCycleModule.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using LightCycle;
 using UnityEngine;
+
 using Rnd = UnityEngine.Random;
 
 /// <summary>
@@ -15,13 +17,39 @@ public class LightCycleModule : MonoBehaviour
     public KMBombModule Module;
     public KMAudio Audio;
 
+    public Material[] LitMats;
+    public Material[] UnlitMats;
+    public MeshRenderer[] Leds;
+
+    private int[] _order;
+    private int _curLed;
+
     void Start()
     {
-        Debug.Log("[Light Cycle] Started");
+        _order = Enumerable.Range(0, 6).ToArray();
+        _order.Shuffle();
+
+        for (int i = 0; i < 6; i++)
+            Leds[i].material = UnlitMats[_order[i]];
+
+        StartCoroutine(Blinkenlights());
+    }
+
+    private IEnumerator Blinkenlights()
+    {
+        _curLed = 0;
+        while (true)
+        {
+            Leds[_curLed].material = LitMats[_order[_curLed]];
+            yield return new WaitForSeconds(.5f);
+            Leds[_curLed].material = UnlitMats[_order[_curLed]];
+
+            _curLed = (_curLed + 1) % 6;
+        }
     }
 
     void ActivateModule()
     {
-        Debug.Log("[Light Cycle] Activated");
+
     }
 }
---
+++ Assets/Ut.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Rnd = UnityEngine.Random;
+
+namespace LightCycle
+{
+    static class Ut
+    {
+        public static T[] NewArray<T>(params T[] array)
+        {
+            return array;
+        }
+
+        public static IList<T> Shuffle<T>(this IList<T> list)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            for (int j = list.Count; j >= 1; j--)
+            {
+                int item = Rnd.Range(0, j);
+                if (item < j - 1)
+                {
+                    var t = list[item];
+                    list[item] = list[j - 1];
+                    list[j - 1] = t;
+                }
+            }
+            return list;
+        }
+
+        public static T PickRandom<T>(this IEnumerable<T> src)
+        {
+            if (src == null)
+                throw new ArgumentNullException("src");
+
+            var arr = src.ToArray();
+            if (arr.Length == 0)
+                throw new InvalidOperationException("Cannot pick a random element from an empty set.");
+            return arr[Rnd.Range(0, arr.Length)];
+        }
+    }
+}
