e50ae549e9c53261b8db136662c315b338eef9a5
Timwi <github@timwi.de>
2017-01-23 17:08:23 +0100

    Light Cycle

---
+++ Assets/LightCycleModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LightCycle;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Light Cycle
+/// Created by Timwi
+/// </summary>
+public class LightCycleModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    void Start()
+    {
+        Debug.Log("[Light Cycle] Started");
+    }
+
+    void ActivateModule()
+    {
+        Debug.Log("[Light Cycle] Activated");
+    }
+}
